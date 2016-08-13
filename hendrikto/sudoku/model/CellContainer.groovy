eb3698076abd3a2812984cdbfec03492d9fbe44b
Hendrik Werner <hendrik.to@gmail.com>
2016-08-13 21:08:14 +0200

    Add ability to store and use candidate values.

--- hendrikto/sudoku/model/CellContainer.groovy
+++ hendrikto/sudoku/model/CellContainer.groovy
@@ -5,6 +5,7 @@ package hendrikto.sudoku.model
  */
 abstract class CellContainer {
     protected final Cell[] cells = new Cell[9]
+    protected final Set<Integer> candidates = new HashSet<>(1..9)
 
     Cell getAt(int i) {
         cells[i]
@@ -12,5 +13,17 @@ abstract class CellContainer {
 
     void putAt(int i, Cell c) {
         cells[i] = c
+    }
+
+    void addCandidate(int c) {
+        candidates.add(c)
+    }
+
+    void removeCandidate(int c) {
+        candidates.remove(c)
+    }
+
+    boolean isCandidate(int c) {
+        candidates.contains(c)
     }
 }
