0cb5326a5cf272ac2214ba7155a806c90a3111ff
Hendrik Werner <hendrik.to@gmail.com>
2016-08-13 19:55:54 +0200

    Add a class to represent columns.

---
+++ hendrikto/sudoku/model/Column.groovy
@@ -0,0 +1,14 @@
+package hendrikto.sudoku.model
+
+/**
+ * @author Hendrik Werner
+ */
+class Column {
+    private Cell[] cells = new Cell[9]
+
+    public Column(Cell... cs) {
+        for (i in 0..<9) {
+            cells[i] = cs[i]
+        }
+    }
+}
