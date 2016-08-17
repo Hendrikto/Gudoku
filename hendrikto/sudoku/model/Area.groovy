1cc8bdd580f0acad535549fe2ab5f1271c4311cd
Hendrik Werner <hendrik.to@gmail.com>
2016-08-17 02:39:51 +0200

    Enable type checking.

--- hendrikto/sudoku/model/Area.groovy
+++ hendrikto/sudoku/model/Area.groovy
@@ -1,10 +1,13 @@
 package hendrikto.sudoku.model
 
+import groovy.transform.TypeChecked
+
 /**
  * @author Hendrik Werner
  */
+@TypeChecked
 abstract class Area {
-    protected final Set<Integer> candidates = 1..9
+    protected final Set<Integer> candidates = 1..9 as Set
 
     void addCandidate(int c) {
         candidates.add c
