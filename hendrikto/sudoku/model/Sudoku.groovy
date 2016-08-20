5e46c0858b3d5179972b7d5628c3b2683572399b
Hendrik Werner <hendrik.to@gmail.com>
2016-08-20 16:27:05 +0200

    Simplify code.

--- hendrikto/sudoku/model/Sudoku.groovy
+++ hendrikto/sudoku/model/Sudoku.groovy
@@ -47,10 +47,7 @@ class Sudoku {
     String toString() {
         StringBuilder sb = new StringBuilder()
         cells.eachWithIndex { Cell cell, int i ->
-            sb.append "$cell "
-            if ((i + 1) % 9 == 0) {
-                sb.setCharAt sb.length() - 1, "\n" as char
-            }
+            sb.append cell append((i + 1) % 9 == 0 ? "\n" : " ")
         }
         sb
     }
