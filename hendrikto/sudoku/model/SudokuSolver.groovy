7ca233374166c201b2bcbaeef07b012d80f369f4
Hendrik Werner <hendrik.to@gmail.com>
2016-08-22 00:03:13 +0200

    Pick single candidates when backtracking.
    * Return a list of changed values when picking single candidates.
    * Pick single candidates when backtracking and clear those cells if it's not a
      solution.

--- hendrikto/sudoku/model/SudokuSolver.groovy
+++ hendrikto/sudoku/model/SudokuSolver.groovy
@@ -10,21 +10,23 @@ class SudokuSolver {
     Sudoku sudoku
 
     void solve() {
-        pickSingleCandidates()
         backtrack()
     }
 
-    private void pickSingleCandidates() {
+    private Set<Cell> pickSingleCandidates() {
+        Set<Cell> changed = []
         boolean progress = true
         while (progress) {
             progress = false
             for (cell in sudoku.cells) {
                 if (cell.empty && cell.candidates.size() == 1) {
                     cell.setValue cell.candidates.head()
+                    changed << cell
                     progress = true
                 }
             }
         }
+        changed
     }
 
     private boolean backtrack() {
@@ -34,9 +36,11 @@ class SudokuSolver {
         Cell cell = sudoku.cells.find { it.empty }
         for (candidate in cell.candidates) {
             cell.setValue candidate
+            Set<Cell> changed = pickSingleCandidates()
             if (backtrack()) {
                 return true
             }
+            changed*.clear()
         }
         cell.clear()
         false
