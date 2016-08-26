f1a4ccb94fce1e379bfe0263f88d7d33991cecb2
Hendrikto <hendrik.to@gmail.com>
2016-08-26 17:11:15 +0200

    Add explicit types.

--- hendrikto/sudoku/model/Sudoku.groovy
+++ hendrikto/sudoku/model/Sudoku.groovy
@@ -14,18 +14,18 @@ class Sudoku {
         Row[] rows = new Row[9]
         Column[] columns = new Column[9]
         Block[][] blocks = new Block[3][3]
-        for (i in 0..<9) {
+        for (int i in 0..<9) {
             rows[i] = new Row()
             columns[i] = new Column()
         }
-        for (x in 0..<3) {
-            for (y in 0..<3) {
+        for (int x in 0..<3) {
+            for (int y in 0..<3) {
                 blocks[x][y] = new Block()
             }
         }
         int i = 0;
-        for (x in 0..<9) {
-            for (y in 0..<9) {
+        for (int x in 0..<9) {
+            for (int y in 0..<9) {
                 cells[i++] = new Cell(
                         this,
                         rows[y],
