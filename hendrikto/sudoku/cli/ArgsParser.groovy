ff06acafa38fc7ec5706efc373ed81d5d8bd57ae
Hendrik Werner <hendrik.to@gmail.com>
2016-09-09 01:21:57 +0200

    Add a class to configure a CliBuilder.

---
+++ hendrikto/sudoku/cli/ArgsParser.groovy
@@ -0,0 +1,15 @@
+package hendrikto.sudoku.cli
+/**
+ * @author Hendrik Werner
+ */
+class ArgsParser {
+    @Delegate final CliBuilder builder = new CliBuilder()
+
+    ArgsParser() {
+        builder.with {
+            usage = "gudoku [options] [Sudoku]"
+            h longOpt: "help", "Print this message."
+            p longOpt: "pretty-print", "Print Sudokus in a human readable form."
+        }
+    }
+}
