ba36cd0524fd2f09a6ae80eb85da4cdfaebc58c2
Hendrik Werner <hendrik.to@gmail.com>
2016-09-09 01:26:49 +0200

    Parse commands and act accordingly.

--- hendrikto/sudoku/cli/Main.groovy
+++ hendrikto/sudoku/cli/Main.groovy
@@ -1,11 +1,26 @@
 package hendrikto.sudoku.cli
 
+import hendrikto.sudoku.model.PrettyStringifier
 import hendrikto.sudoku.model.Sudoku
+import hendrikto.sudoku.model.SudokuSolver
 
 /**
  * @author Hendrik Werner
  */
 class Main {
     static void main(String[] args) {
+        ArgsParser parser = new ArgsParser()
+        OptionAccessor options = parser.parse args
+        if (options.arguments().empty || options.help) {
+            parser.usage()
+            return
+        }
+        Sudoku sudoku = new Sudoku(options.arguments().head())
+        if (options."pretty-print") {
+            sudoku.stringifier = new PrettyStringifier()
+            println "Read Sudoku:\n\n$sudoku\n\nSolving...\n\n"
+        }
+        new SudokuSolver(sudoku: sudoku).solve()
+        print sudoku
     }
 }
