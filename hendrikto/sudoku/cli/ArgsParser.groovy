e805362c2bd270b7801ff5148b04119ecd9c966b
Hendrik Werner <hendrik.to@gmail.com>
2016-09-10 03:58:26 +0200

    Add a footer containing a link to the project's Github page.

--- hendrikto/sudoku/cli/ArgsParser.groovy
+++ hendrikto/sudoku/cli/ArgsParser.groovy
@@ -8,6 +8,7 @@ class ArgsParser {
     ArgsParser() {
         builder.with {
             usage = "gudoku [options] <Sudoku>"
+            footer = "For more information visit: github.com/Hendrikto/Gudoku/"
             h longOpt: "help", "Print this help message."
             p longOpt: "pretty-print", "Print output in a human readable form."
         }
