a4a21b78d4a256df7882647e6a0b379070ac6fa9
Hendrik Werner <hendrik.to@gmail.com>
2016-09-13 02:13:43 +0200

    Simplify code.

--- hendrikto/sudoku/model/Cell.groovy
+++ hendrikto/sudoku/model/Cell.groovy
@@ -1,8 +1,6 @@
 package hendrikto.sudoku.model
 
 import groovy.transform.CompileStatic
-import groovy.transform.stc.ClosureParams
-import groovy.transform.stc.SimpleType
 
 /**
  * @author Hendrik Werner
@@ -12,16 +10,14 @@ class Cell {
     static final IntRange ALLOWED_VALUES = 1..9
 
     final Sudoku sudoku
-    final Area row
-    final Area column
-    final Area block
+    final Area[] areas = new Area[3]
     int value
 
     Cell(final Sudoku sudoku, final Area row, final Area column, final Area block) {
         this.sudoku = sudoku
-        this.row = row
-        this.column = column
-        this.block = block
+        this.areas[0] = row
+        this.areas[1] = column
+        this.areas[2] = block
         sudoku.empty << this
     }
 
@@ -29,27 +25,19 @@ class Cell {
         assert newValue in ALLOWED_VALUES
         clear()
         value = newValue
-        forAllAreas { it.remove value }
+        areas*.remove value
         sudoku.empty.remove this
     }
 
     void clear() {
         if (value) {
-            forAllAreas { it << value }
+            areas*.add value
             value = 0
             sudoku.empty.add 0, this
         }
     }
 
     Set<Integer> getCandidates() {
-        row.intersect column intersect block
-    }
-
-    private void forAllAreas(
-            @ClosureParams(value = SimpleType, options = "hendrikto.sudoku.model.Area") final Closure closure
-    ) {
-        closure row
-        closure column
-        closure block
+        areas[0].intersect areas[1] intersect areas[2]
     }
 }
